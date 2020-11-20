a469cda3273e8fb3f78c0b3a5b1b081c319f36de
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-11-20 14:57:06 +0100

    Creata funzione per sommare i numeri
    Creata funzione per sommare i numeri

--- pariedispari/assets/js/main.js
+++ pariedispari/assets/js/main.js
@@ -1,16 +1,21 @@
 //L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-//sceltaUtente = prompt("Scegli pari o dispari")
+sceltaUtente = prompt("Scegli pari o dispari")
 //console.log(sceltaUtente);
-//numeroUtente = prompt("scegli un numero da 1 a 5")
+numeroUtente = Number(prompt("scegli un numero da 1 a 5"))
 //console.log(numeroUtente);
 //Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 function numeroRandom(min, max) {
     return Math.floor(Math.random()* (max - min) + min)
 }
 
-var numeroComputer = numeroRandom(1, 6)
+var numeroComputer = Number(numeroRandom(1, 6))
 
-console.log(numeroComputer);
+//console.log(numeroComputer);
 //Sommiamo i due numeri
+function sommaNumeri(n1, n2) {
+    return (n1 + n2)
+}
+var somma = sommaNumeri(numeroUtente, numeroComputer)
+console.log(somma);
 //Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 //Dichiariamo chi ha vinto.
