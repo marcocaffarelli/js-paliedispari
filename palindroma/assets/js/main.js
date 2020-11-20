6ff4dd94b698d6814e455e57616e0b2f721f5f3f
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-11-20 14:33:43 +0100

    Creata una funzione per verificare se la parola è palindroma
    Creata una funzione per verificare se la parola è palindroma

--- palindroma/assets/js/main.js
+++ palindroma/assets/js/main.js
@@ -1,5 +1,18 @@
 //Chiedere all’utente di inserire una parola
-var parola = prompt("Inserisci una parola")
-console.log(parola);
+var parolaUtente = prompt("Inserisci una parola")
+//console.log(parolaUtente);
 
 //Creare una funzione per capire se la parola inserita è palindroma
+//funzione controllo 
+function ctrlPalindroma(parola){
+    // se la parola è uguale alla parola scritta al contrario allora vero
+    if (parola === parola.split("").reverse().join("")){
+    return true;
+    }
+}
+
+var ctrlParola = ctrlPalindroma(parolaUtente)
+
+if (ctrlParola) {
+    console.log("la parola è palindroma");
+}
