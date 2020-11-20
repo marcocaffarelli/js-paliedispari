748086150bff70034b3e1dafd977748ccd992016
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-11-20 13:59:14 +0100

    Inserita variabile per chiedere una parola
    Inserita variabile per chiedere una parola

--- palindroma/assets/js/main.js
+++ palindroma/assets/js/main.js
@@ -1,2 +1,5 @@
 //Chiedere all’utente di inserire una parola
+var parola = prompt("Inserisci una parola")
+console.log(parola);
+
 //Creare una funzione per capire se la parola inserita è palindroma
