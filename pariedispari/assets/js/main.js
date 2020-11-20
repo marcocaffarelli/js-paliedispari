7b08ca51fd69fceb9b6cefd9f1a67c661221a6bb
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-11-20 14:51:01 +0100

    Creata funzione numero casuale
    Creata funzione numero casuale

--- pariedispari/assets/js/main.js
+++ pariedispari/assets/js/main.js
@@ -1,7 +1,16 @@
 //L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-sceltaUtente = prompt("Scegli pari o dispari")
-numeroUtente = prompt("scegli un numero da 1 a 5")
+//sceltaUtente = prompt("Scegli pari o dispari")
+//console.log(sceltaUtente);
+//numeroUtente = prompt("scegli un numero da 1 a 5")
+//console.log(numeroUtente);
 //Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+function numeroRandom(min, max) {
+    return Math.floor(Math.random()* (max - min) + min)
+}
+
+var numeroComputer = numeroRandom(1, 6)
+
+console.log(numeroComputer);
 //Sommiamo i due numeri
 //Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 //Dichiariamo chi ha vinto.
