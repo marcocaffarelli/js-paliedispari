e9b03340b6debcee01b18f4b60d276d55336d9cc
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-11-20 16:12:45 +0100

    Aggiunto toLowerCase a sceltaUtente
    Aggiunto toLowerCase a sceltaUtente

--- pariedispari/assets/js/main.js
+++ pariedispari/assets/js/main.js
@@ -1,34 +1,37 @@
+//variabili
+var sceltaUtente, numeroUtente, numeroComputer, somma, pari, dispari
 //L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-sceltaUtente = prompt("Scegli pari o dispari")
+sceltaUtente = prompt("Scrivi pari o dispari", "pari o dispari");
 //console.log(sceltaUtente);
-numeroUtente = Number(prompt("scegli un numero da 1 a 5"))
+sceltaUtente = sceltaUtente.toLowerCase();
+numeroUtente = Number(prompt("scrivi un numero da 1 a 5"));
 //console.log(numeroUtente);
 //Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 function numeroRandom(min, max) {
-    return Math.floor(Math.random()* (max - min) + min)
-}
+    return Math.floor(Math.random()* (max - min) + min);
+};
 
-var numeroComputer = Number(numeroRandom(1, 6))
+numeroComputer = Number(numeroRandom(1, 6));
 
 //console.log(numeroComputer);
 //Sommiamo i due numeri
 function sommaNumeri(n1, n2) {
-    return (n1 + n2)
-}
-var somma = sommaNumeri(numeroUtente, numeroComputer)
+    return (n1 + n2);
+};
+somma = sommaNumeri(numeroUtente, numeroComputer);
 console.log(somma);
 //Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 if (somma % 2 == 0){
-    console.log("numero pari");
-    var pari = "pari"
+    console.log("Numero pari");
+    pari = "pari";
 }else {
-    console.log("numero dispari");
-    var dispari = "dispari"
-}
+    console.log("Numero dispari");
+    dispari = "dispari";
+};
 //Dichiariamo chi ha vinto.
 if (pari == sceltaUtente || dispari == sceltaUtente){
     console.log("Complimenti hai vinto");
 }else{
     console.log("Mi dispiace hai perso");
-}
+};
 
